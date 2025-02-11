bc81b84f4c1b42683ce0dea6b3e2dea740ab49b4
DaDMraZ <k79176241840@gmail.com>
2025-02-11 10:21:29 +0400

    Add files via upload
    z cfvsq rhenjq yf inhfayjq gkjiflrt

--- GAME/main.cpp
+++ GAME/main.cpp
@@ -1,70 +1,88 @@
-//========================================================================
-//! @file       Main.cpp
-//{=======================================================================
-//! 
-//! @brief      <Заголовок>\n
-//! @brief      <Подзаголовок>
-//! 
-//! @version    [Version 0.01 alpha, build 1]
-//! @author     Copyright (C) <Автор>, <Год> (<Имя> <Почта>)
-//! @date       <Дата>
-//! 
-//! @par        Протестировано
-//!           - (TODO: список платформ)
-//! 
-//! @todo     - (TODO: список ближайших планов по этому файлу)
-//! 
-//! @bug      - (TODO: список найденных ошибок в этом файле)
-//! 
-//! @par        История изменений файла
-//!           - Версия 0.01 Alpha
-//!             - Только что созданный файл
-//! 
-//}=======================================================================
 
 #include "TXLib.h"
+struct Truba
+{
+  int x;
+  int y;
+  int w;
+  int h;
+  HDC image;
+  HDC image_up;
+  HDC image_down;
+
+
+
+ void draw()
+    {
+      txTransparentBlt (txDC(),x,y,w ,h, image,0,0,TX_WHITE);
+
+    }
+
+
+};
+
+
+struct Birsd
+{
+   int x;
+   int y;
+   int w;
+   int h;
+   HDC image;
+   HDC image_jump;
+   HDC image_fly;
+
+
+
+
+ void draw()
+    {
+      txTransparentBlt (txDC(),x,y,w ,h, image,0,0,TX_WHITE);
+
+    }
+
+
+ };
+
+ //////                          //////////////////////////////////////////////////ghbdtn vtyz pjden rbhbk vyt 13 ktn yj xthtp 5 vtczxtd ,eltn 14 z bp ekmzyjdcrf b hjbkcz nj;t nfv vj` [j,,b 'nj evyst cnhfntubb yfghbvth rkti hjzkm bkb ljnf dtlm vyt yhfdbncz levfnm////////////////////////////                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 шутка                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          а может и нет//////////////////////////////////////////////////////////                                                                                                                                                                                                                                                                                                                                                                                                                                                   //////////////////////////////////////////////////////////////////мне лень уже  /                        //////////////////////////////////////////////////////////////////////////* /* /* /*/ /* / /*/ */ */ */ */////////////////////////////////////////
+
 
 int main()
     {
-    txCreateWindow (800, 600);
-
-    txSetColor (TX_WHITE);
-    txSetFillColor (TX_TRANSPARENT);
-    txRectangle (10, 10, 790, 590);
-
-    txSetColor (TX_LIGHTCYAN);
-    txEllipse (200, 150, 600, 450);
-    txEllipse (245, 150, 555, 450);
-    txEllipse (290, 150, 510, 450);
-    txEllipse (330, 150, 470, 450);
-    txEllipse (375, 150, 425, 450);
-    txEllipse (200, 150, 600, 450);
-    txEllipse (200, 190, 600, 410);
-    txEllipse (200, 230, 600, 370);
-    txEllipse (200, 270, 600, 330);
-    txLine    (200, 300, 600, 300);
-
-    txSetColor (TX_LIGHTGREEN);
-    txSelectFont ("Times", 60);
-    txSetTextAlign (TA_CENTER);
-    txTextOut (400, 480, "Hello, world!");
-
-    txSetColor (TX_YELLOW);
-    txSetFillColor (TX_YELLOW);
-    txLine   (385, 135, 385, 120);
-    txLine   (385, 135, 375, 150);
-    txLine   (385, 135, 395, 150);
-    txLine   (385, 125, 375, 135);
-    txLine   (385, 125, 400, 120);
-    txCircle (385, 115, 6);
-
-    txSetFillColor (TX_TRANSPARENT);
-    txLine (400, 75, 400, 150);
-    txRectangle (400, 75, 450, 115);
-    txSelectFont ("Times", 20);
-    txTextOut (425, 85, "C++");
-
+    txCreateWindow (1000, 600);
     txTextCursor (false);
-    return 0;
+
+
+
+ while(!GetAsyncKeyState(VK_ESCAPE))
+ {
+     txClear();
+     txBegin();
+
+Birsd birsd = {225,225,225, 225,txLoadImage("fly.bmp"),txLoadImage("jump.bmp"),txLoadImage("fly.bmp")};
+Truba truba = {225,225,225, 225,txLoadImage("truba_up.bmp"),txLoadImage("truba_up.bmp"),txLoadImage("truba_down.bmp")};
+
+birsd.draw();
+
+
+
+
+
+  if(GetAsyncKeyState(VK_SPACE))
+  {
+  birsd.y -= 70;
+  birsd.image = birsd.image_jump;
+  }
+
+ birsd.y += 15;
+ birsd.image =birsd.image_fly ;
+
+
+      ///End();///
+      Sleep(20);
     }
 
+    //DeleteDC(birsd.image);
+    txDisableAutoPause();
+    return 0;
+    }
